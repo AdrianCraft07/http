9cf77062953645a2c322a37e1ce4cd4b5d913ea1
AdrianCraft07 <proadrian684@gmail.com>
2022-09-06 22:47:22 -0500

    remove http server and edit d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,32 +1,4 @@
-import http from 'http';
-
 import request from './request';
-import Router from './router';
-import static from './static';
-import db from './db';
+import DB from './DB';
 
-import { requestT, method } from './types';
-
-interface USE {
-  name: String | undefined;
-  METHODS: {
-    get: [String, requestT][];
-    put: [String, requestT][];
-    post: [String, requestT][];
-    delete: [String, requestT][];
-  };
-}
-interface Server extends http.Server {
-  use(callback: USE): void;
-  run(): void;
-  get: method;
-  put: method;
-  post: method;
-  delete: method;
-}
-declare function HTTP(): Server;
-HTTP.request = request;
-HTTP.Router = Router;
-HTTP.static = static;
-HTTP.db = db;
-export = HTTP;
+export { request, DB };
