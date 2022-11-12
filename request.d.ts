9af7012bad8d9552c1e7458a2512523391ebd0b3
AdrianCraft07 <proadrian684@gmail.com>
2022-11-11 19:14:44 -0600

    request response

--- request.d.ts
+++ request.d.ts
@@ -2,5 +2,9 @@ declare const _default: (url: string, { method, headers, body }?: {
     method?: string | undefined;
     headers?: {} | undefined;
     body?: string | undefined;
-}) => Promise<{buffer():Buffer,text():string,json():Object}>;
+}) => Promise<{
+    json: () => Object;
+    text: () => string;
+    buffer: () => Buffer;
+}>;
 export = _default;
